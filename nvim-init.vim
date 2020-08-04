0eafe3635edb5edbe6e2216735d28cab12bd7712
Kiril Momchilov <momchilov@gmail.com>
2020-08-04 23:30:33 +0100

    update

---
+++ nvim-init.vim
@@ -0,0 +1,6 @@
+set background=dark
+set nu
+call plug#begin('~/.vim/plugged')
+Plug 'junegunn/vim-easy-align'
+Plug 'rchain-community/rholang-syntax-highlighting', {'rtp': 'vim'}
+call plug#end()
---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd $HOME
+
+# Install packages
+sudo apt install neovim nmap zsh git tmux
+
+# Set default shell to zsh
+chsh -s $(which zsh)
+
+# neovim
+nvim_confdir=$HOME/.config/nvim
+mkdir -p $nvim_confdir
+ln -sf ../../dotfiles/nvim-init.vim $nvim_confdir/init.vim
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ln -sf dotfiles/zshrc $HOME/.zshrc
+source $HOME/.zshrc
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
