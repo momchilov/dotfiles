adf65cee2f6233249e887879b4b7932100fd7da3
Kiril Momchilov <momchilov@gmail.com>
2020-08-05 20:48:13 +0100

    setup: adding tmux-related stuff

--- setup.sh
+++ setup.sh
@@ -8,6 +8,11 @@ sudo apt install neovim nmap zsh git tmux
 # Set default shell to zsh
 chsh -s $(which zsh)
 
+# tmux config
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+
 # neovim
 nvim_confdir=$HOME/.config/nvim
 mkdir -p $nvim_confdir
@@ -19,4 +24,3 @@ ln -sf dotfiles/zshrc $HOME/.zshrc
 source $HOME/.zshrc
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
